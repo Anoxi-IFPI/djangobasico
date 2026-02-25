5954213a5c5c923fe22e42c13fe18bff154094cc
Antonio José <antonioalves073d@gamial.com>
2026-02-25 13:15:27 -0300

    Criando rotas

--- djangobasico/urls.py
+++ djangobasico/urls.py
@@ -20,5 +20,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('produtos/', include('produtos.urls')),
+    path('clientes/', include('clientes.urls')),
 
 ]
