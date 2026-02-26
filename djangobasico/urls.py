9806449c1a6fa4f9549ac8ac69282b7e06725b92
Antonio José <antonioalves073d@gamial.com>
2026-02-26 10:29:32 -0300

    Aprendendo mais sobre rotas

--- djangobasico/urls.py
+++ djangobasico/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-
+from inicio.views import index
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', index),
     path('produtos/', include('produtos.urls')),
     path('clientes/', include('clientes.urls')),
+    path('inicio/', include('inicio.urls')),
 
 ]
