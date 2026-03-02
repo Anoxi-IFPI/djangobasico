1cbb60832ee427a16749711eae9e38872c19c88b
Antonio José <antonioalves073d@gamial.com>
2026-03-02 11:47:02 -0300

    Mais sobre templates

--- produtos/urls.py
+++ produtos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pagina_produtos),
+    path('', views.index),
     path('celulares/', views.celulares),
 
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
-def pagina_produtos(request):
-    return HttpResponse('Página de produtos')
+def index(request):
+    
+    idade = 15 
+    tipo = ''
+    
+    if idade < 18:
+        tipo = 'Menor de idade'
+    elif idade >= 12 and idade <= 18:
+        tipo = 'Adolescente'
+    else:
+        tipo = 'Adulto'
+            
+    context = {
+        'nome': 'Django básico',
+        'idade': 15,
+        'tipo': tipo,
+        'ultimo_acesso': '10/10/2030',
+        'produtos': [  # Usei minúsculo para facilitar
+            {'nome': 'Notebook Acer', 'preco': 'R$ 2.500,00'},
+            {'nome': 'Iphone', 'preco': 'R$ 5.000,00'},
+            {'nome': 'Samsung Galaxy', 'preco': 'R$ 3.000,00'},
+        ]
+    }
+    return render(request, 'index.html', context)
 
 def celulares(request):
-    return HttpResponse('Página de celulares')
+    return render(request,'celulares.html')
+
